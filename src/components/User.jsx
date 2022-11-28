0ece58267215535e2df2ffe8fe7b564d16c9f723
didiraja <didiraja@hotmail.com>
2022-11-28 00:26:32 -0300

    refactor: 💡 update prop validation

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,20 +5,13 @@ function User(prop) {
   return (
     <div className="user text-sm p-3 mb-3">
       <div>
-        {/* {prop.username && <p className="text-md font-bold inline">{prop.username}</p>}
-        {prop.email && <p className="text-sm italic inline ml-1">({prop.email})</p>} */}
         <p className="text-md font-bold inline">{prop.username}</p>
         <p className="text-sm italic inline ml-1">({prop.email})</p>
       </div>
 
       <div className="ml-3">
-        {/*
-        {prop.name && <p>Name: {prop.name}</p>}
-        {prop.website && <p>Site: {prop.website}</p>}
-        */}
         <p>Name: {prop.name}</p>
         <p>Site: {prop.website}</p>
-        {/* <p>Company: {prop.company.name}</p> */}
         {prop.company && <p>Company: {prop.company.name}</p>}
       </div>
     </div>
